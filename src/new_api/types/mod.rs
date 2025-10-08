69375489e29896109a8d0802524edf00f51ef9c5
Tobias Bieniek <tobias@bieniek.cloud>
2025-10-08 18:26:56 +0200

    Add `RawItemData` struct for low-level API

--- src/new_api/types/mod.rs
+++ src/new_api/types/mod.rs
@@ -1,9 +1,11 @@
 //! Type definitions for new API
 
 mod point_op;
+mod raw_item_data;
 
 // Re-export existing types that we'll reuse
 pub use crate::types::{AltStyle, CubClass, CubStyle, DaysActive, ExtendedType};
 pub use crate::types::{Header, NotamCodes, NotamScope, NotamTraffic, NotamType};
 
 pub use point_op::PointOp;
+pub use raw_item_data::RawItemData;
